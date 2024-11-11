865303ceb34a22e0a3b66bebdb19c09b33304bbd
Michael Gerasymenko <michael.gerasymenko@tamara.co>
2024-11-11 09:15:03 +0100

    Compilation, warning reporting

--- Sources/PeckerKit/Reporters/XcodeReporter.swift
+++ Sources/PeckerKit/Reporters/XcodeReporter.swift
@@ -2,21 +2,9 @@ import Foundation
 import SwiftSyntax
 
 public struct XcodeReporter: Reporter {
-    
     public func report(_ configuration: Configuration, sources: [SourceDetail]) {
-        fatalError("What should I do?")
-//        let diagnosticEngine = makeDiagnosticEngine()
-//        for source in sources {
-//            let message = Diagnostic.Message(.warning, "Pecker: \(source.name) was never used; consider removing it")
-//            diagnosticEngine.diagnose(message, location: source.location.ssLocation, actions: nil)
-//        }
+        for source in sources {
+            print("\(source.name): warning: Pecker: The file was never used; consider removing it")
+        }
     }
 }
-//
-///// Makes and returns a new configured diagnostic engine.
-//private func makeDiagnosticEngine() -> DiagnosticEngine {
-//  let engine = DiagnosticEngine()
-//  let consumer = PrintingDiagnosticConsumer()
-//  engine.addConsumer(consumer)
-//  return engine
-//}
